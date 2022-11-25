3ea461b0530272ffede56c1e52a87febab864848
Onur ÖZEL <1030516465@erciyes.edu.tr>
2022-11-25 22:42:05 +0300

    Road Placer Implemented

---
+++ Assets/[GAME]/Scripts/Extensions/RoadScaler.cs
@@ -0,0 +1,34 @@
+using PathCreation;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _GAME_.Scripts.Extensions
+{
+    public class RoadScaler : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [Header("Components")] [SerializeField]
+        private PathCreator pathCreator;
+
+        [SerializeField] private Transform road;
+
+        #endregion
+
+        #region Public Methods
+
+        [Button("Scale Road")]
+        public void ScaleRoad()
+        {
+            Vector3 localScale = road.localScale;
+            localScale = new Vector3(localScale.x, localScale.y, pathCreator.path.length / 2 + 10);
+            road.localScale = localScale;
+
+            Vector3 position = road.position;
+            position = new Vector3(position.x, position.y, position.z - 20);
+            road.position = position;
+        }
+
+        #endregion
+    }
+}
